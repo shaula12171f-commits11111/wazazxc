de3cc6d53140936a1763dc1c98edfb0ed6229091
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-05 13:07:33 -0500

    Add files via upload

---
+++ galerias/nino/nino_contenedor.js
@@ -0,0 +1,10 @@
+// nino_contenedor.js
+// Junta todas las subgalerias de Nino.
+// Para agregar una subgaleria nueva: crea sus archivos (nino_sub_X.js y .datos.js),
+// enlazalos en index.html, y agregalos aqui abajo.
+
+const nino = {
+  titulo: "Nino",
+  portada: "",
+  subgalerias: [nino_sub_1]
+};
