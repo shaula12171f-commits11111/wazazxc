225a7c459b18c8e2943d0b3b034bbc6071518a51
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-05 13:09:04 -0500

    Delete galerias directory

--- galerias/ichika/ichika_sub_1.js
+++
@@ -1,8 +0,0 @@
-// ichika_sub_1.js
-// Solo logica. No edites este archivo para cambiar contenido, edita ichika_sub_1.datos.js.
-
-const ichika_sub_1 = {
-  titulo: ichika_sub_1_datos.titulo,
-  portada: ichika_sub_1_datos.portada,
-  items: ichika_sub_1_datos.items
-};
--- galerias/nino/nino_sub_1.js
+++
@@ -1,8 +0,0 @@
-// nino_sub_1.js
-// Solo logica. No edites este archivo para cambiar contenido, edita nino_sub_1.datos.js.
-
-const nino_sub_1 = {
-  titulo: nino_sub_1_datos.titulo,
-  portada: nino_sub_1_datos.portada,
-  items: nino_sub_1_datos.items
-};
