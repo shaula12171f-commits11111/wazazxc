e6556d99102a972f4fe2247f452edbc1ed2431ec
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-05 13:16:57 -0500

    Add files via upload

---
+++ contenedores.js
@@ -0,0 +1,9 @@
+// contenedores.js
+// Junta las chicas dentro de "galerias". Por ahora solo esta Ichika.
+
+const categorias = {
+  galerias: {
+    titulo: "Galerías",
+    subgalerias: [ichika]
+  }
+};
