12abfc8e43819371e7e223b13a0c39f6c7fc713a
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-05 13:05:55 -0500

    Add files via upload

---
+++ galerias/ichika/ichika_contenedor.js
@@ -0,0 +1,10 @@
+// ichika_contenedor.js
+// Junta todas las subgalerias de Ichika.
+// Para agregar una subgaleria nueva: crea sus archivos (ichika_sub_X.js y .datos.js),
+// enlazalos en index.html, y agregalos aqui abajo.
+
+const ichika = {
+  titulo: "Ichika",
+  portada: "",
+  subgalerias: [ichika_sub_1]
+};
