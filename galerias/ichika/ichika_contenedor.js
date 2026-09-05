7befe4cc6d7a5f0a3fb4deb09e1b18de5f24971d
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-05 13:20:23 -0500

    Update ichika_contenedor.js

--- galerias/ichika/ichika_contenedor.js
+++ galerias/ichika/ichika_contenedor.js
@@ -3,6 +3,6 @@
 
 const ichika = {
   titulo: "Ichika",
-  portada: "",
+  portada: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD9/main/imagenes/img_1773421659176.jpg",
   subgalerias: [ichika_sub_1]
 };
